b5ab6401223a8a2a014311c33f4cdb620bcce087
Beckett Frey <beckett.frey@gmail.com>
2024-11-09 15:28:59 -0600

    dev-setup

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });

2e272ec49846f2b85b1a6ceda4afe05f78725423
Kaleb Kohn <kohnkaleb@gmail.com>
2024-11-09 21:10:29 -0600

    pretty front-end

--- frontend/src/components/EmailList.jsx
+++ frontend/src/components/EmailList.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
 
 const EmailList = () => {
     const [emails, setEmails] = useState([]);
+    const [checked, setChecked] = React.useState([0]);
 
     useEffect(() => {
         fetch('http://localhost:3001/emails')
@@ -11,17 +17,66 @@ const EmailList = () => {
             .then((data) => setEmails(data));
     }, []);
 
+    const handleToggle = (value) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+    
+        if (currentIndex === -1) {
+          newChecked.push(value);
+        } else {
+          newChecked.splice(currentIndex, 1);
+        }
+    
+        setChecked(newChecked);
+    };
+
+    const handleSelectAll = () => {
+        const allChecked = emails.map((email, index) => index);
+        setChecked(allChecked);
+    };
+
+    const handleDeselectAll = () => {
+        setChecked([]);
+    };
+
     return (
-        <Container>
-            <h2>Emails</h2>
-            <ListGroup>
-                {emails.map((email, index) => (
-                <ListGroup.Item key={index}>
-                    <h3>{email.headers.from}</h3>
-                </ListGroup.Item>
-                ))}
-            </ListGroup>
-        </Container>
+        <>
+            <div>
+                <Button onClick={handleSelectAll} variant="contained" color="primary">
+                    Select All
+                </Button>
+                <Button onClick={handleDeselectAll} variant="contained" color="secondary" style={{ marginLeft: '10px' }}>
+                    Deselect All
+                </Button>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    {emails.map((email, index) => {
+                        const labelId = `checkbox-list-label-${index}`;
+
+                        return (
+                            <ListItem
+                                key={index}
+                                disablePadding
+                            >
+                                <ListItemButton role={undefined} onClick={handleToggle(index)}>
+                                    <ListItemIcon>
+                                        <Checkbox
+                                            edge="start"
+                                            checked={checked.indexOf(index) !== -1}
+                                            tabIndex={-1}
+                                            disableRipple
+                                            inputProps={{ 'aria-labelledby': labelId }}
+                                        />
+                                    </ListItemIcon>
+                                    <ListItemText id={labelId} primary={email.headers.from} />
+                                </ListItemButton>
+                            </ListItem>
+                        );
+                    })}
+                </List>
+            </div>
+        </>
     );
 };
 
